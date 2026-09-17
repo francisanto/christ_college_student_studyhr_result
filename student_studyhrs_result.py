a554f344671f566da5a4e8415b945523cecc4424
FRANCIS ANTO <145269457+francisanto@users.noreply.github.com>
2026-09-17 10:39:55 +0530

    Update student_studyhrs_result.py

--- student_studyhrs_result.py
+++ student_studyhrs_result.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import joblib
-model=joblib.load("Christ_College_francisanto_Deplyment.pkl.pkl")
+model = joblib.load("Christ_College_francisanto_Deplyment.pkl")
 st.title("Student Pass/Fail based on Study Hours")
 hours=st.number_input("Enter Study Hours",min_value=0.0,max_value=15.0,value=5.0)
 if st.button("Predict"):
