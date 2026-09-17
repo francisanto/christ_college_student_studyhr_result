d6f4763a79f5bf11b6b621fe1aa99cdd156f0943
FRANCIS ANTO <145269457+francisanto@users.noreply.github.com>
2026-09-17 10:25:02 +0530

    Update model file name for predictions

--- student_studyhrs_result.py
+++ student_studyhrs_result.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import joblib
-model=joblib.load("logistic_regression_students_studyhours_model.pkl")
+model=joblib.load("Christ_College_francisanto_Deplyment.pkl")
 st.title("Student Pass/Fail based on Study Hours")
 hours=st.number_input("Enter Study Hours",min_value=0.0,max_value=15.0,value=5.0)
 if st.button("Predict"):
