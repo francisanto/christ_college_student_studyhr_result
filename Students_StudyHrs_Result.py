29122b7d1a26f36a07a3189dfc5689f96a164d9b
FRANCIS ANTO <145269457+francisanto@users.noreply.github.com>
2026-09-17 11:04:28 +0530

    Include attendance input in prediction data
    Added attendance input for prediction model.

--- Students_StudyHrs_Result.py
+++ Students_StudyHrs_Result.py
@@ -16,11 +16,18 @@ hours = st.number_input(
     value=5.0
 )
 
+
+attendance = st.number_input(
+    "Enter Attendance",
+    min_value = 0.0,
+    max_value = 100.0,
+    value = 75.0)
+
 if st.button("Predict"):
 
     student = pd.DataFrame(
-        [[hours]],
-        columns=["StudyHours"]
+        [[hours,attendance]],
+        columns=["StudyHours","Attendance"]
     )
 
     prediction = model.predict(student)
@@ -37,3 +44,4 @@ if st.button("Predict"):
         round(probability[0][1] * 100, 2),
         "%"
     )
+
