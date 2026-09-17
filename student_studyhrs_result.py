4b9949677445d1d6f0e8c9b12788457270416a30
FRANCIS ANTO <145269457+francisanto@users.noreply.github.com>
2026-09-17 09:40:30 +0530

    Fix import and implement prediction feature

--- student_studyhrs_result.py
+++ student_studyhrs_result.py
@@ -1,1 +1,11 @@
-import stremlit as lt
+import streamlit as st
+import joblib
+model=joblib.load("logistic_regression_students_studyhours_model.pkl")
+st.title("Student Pass/Fail based on Study Hours")
+hours=st.number_input("Enter Study Hours",min_value=0.0,max_value=15.0,value=5.0)
+if st.button("Predict"):
+  prediction=model.predict([[hours]])
+  if prediction[0]==1:
+    st.success("Student will PASS")
+  else:
+    st.error("Student will FAIL")
